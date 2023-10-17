069cef68c6da254b910797f98dd6b5238b1d9d49
PSYCHNERD2512 <jatinsinghal2003@gmail.com>
2023-10-17 13:26:36 +0530

    Added key for booking

--- steps/lib/eventsPage.dart
+++ steps/lib/eventsPage.dart
@@ -1,92 +1,199 @@
 import 'package:flutter/material.dart';
-import 'package:steps/loginPage.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-//Code for events page
 
-class eventsPage extends StatefulWidget {
-  const eventsPage({Key? key, required this.title}) : super(key: key);
+void main() => runApp(MaterialApp(home: EventsPage(title: 'Event Page')));
+
+class EventsPage extends StatefulWidget {
+  const EventsPage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
-  State<eventsPage> createState() => _eventsPageState();
+  State<EventsPage> createState() => _EventsPageState();
 }
 
-class _eventsPageState extends State<eventsPage> {
+class _EventsPageState extends State<EventsPage> {
   final List<String> eventNames = [
     'Life Lessons With Top G',
     'Quantum Computing...What\'s that??',
     'Ludo Championship',
     'AI Girlfriends: An undiscovered phenomenon'
   ];
-  final List<bool> bookings = [false, false, false, false];
-  @override
-  Widget build(
-    BuildContext context,
-  ) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text("Book Your Seats"),
-        ),
-        body: SafeArea(
-          child: ListView.builder(
-              itemCount: eventNames.length + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  loginPage(title: "Log In Page")));
-                    },
-                    child: Text("LogOut"),
-                  );
-                } else {
-                  bool isBooked = bookings[index - 1];
-                  return Card(
-                    color: Color.fromRGBO(246, 244, 235, 1),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 0, right: 12, top: 5),
-                              child: Image.network(
-                                "https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80",
-                                height: 150,
-                                width: 150,
-                              ),
-                            ),
-                            Flexible(
-                                child: Text(
-                              eventNames[index - 1],
-                              softWrap: true,
-                              maxLines: 2,
-                            )),
-                          ],
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              if (!isBooked) {
-                                print("You're Booked!!");
-                                bookings[index - 1] = true;
-                              } else {
-                                print("QR Screen");
-                                //Backend Connection for storing ticket info and fetching a qr
 
-                              }
-                            });
-                          },
-                          child: Text(isBooked ? "View Ticket" : "Book Seat"),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              }),
-        ));
+  final List<bool> bookings = [false, false, false, false];
+  Map<int, String> existingKeys = {};
+
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  Future<void> _storeBookingKeys(Map<int, String> bookingKeys) async {
+    String formattedKeys = bookingKeys.entries.map((entry) {
+      return '${entry.key}:${entry.value}';
+    }).join(',');
+
+    await _storage.write(key: 'booking_keys', value: formattedKeys);
+  }
+
+  Future<void> _getStoredKeys() async {
+    Map<int, String> storedKeys = await _getBookingKeys();
+    setState(() {
+      existingKeys = storedKeys;
+      for (int i = 0; i < eventNames.length; i++) {
+        bookings[i] = existingKeys.containsKey(i + 1);
+      }
+    });
+  }
+
+  Future<Map<int, String>> _getBookingKeys() async {
+    String? encodedKeys = await _storage.read(key: 'booking_keys');
+    if (encodedKeys != null) {
+      List<String> keyPairs = encodedKeys.split(',');
+      Map<int, String> decodedKeys = {};
+
+      keyPairs.forEach((keyPair) {
+        List<String> parts = keyPair.split(':');
+        if (parts.length == 2) {
+          int key = int.tryParse(parts[0].trim()) ?? 0;
+          String value = parts[1].trim();
+          decodedKeys[key] = value;
+        }
+      });
+
+      return decodedKeys;
+    } else {
+      return {};
+    }
+  }
+
+  String _generateBookingKey(int userId, List<int> eventIds) {
+    DateTime now = DateTime.now();
+    String bookingKey = '$userId-${eventIds.join('-')}-00000000';
+    print('Booking Key: $bookingKey'); // Print booking key to terminal
+    return bookingKey;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getStoredKeys();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text("Book Your Seats"),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: eventNames.length,
+          itemBuilder: (context, index) {
+            int eventId = index + 1;
+
+            return EventCard(
+              eventName: eventNames[index],
+              isBooked: bookings[index],
+              onBookPressed: () async {
+                // Assume you have a user ID and event IDs
+                int userId = 1; // Replace with the actual user ID
+                List<int> eventIds = [eventId, eventId+1]; // Event ID(s) for the booking
+
+                String bookingKey = _generateBookingKey(userId, eventIds);
+
+                existingKeys[eventId] = bookingKey;
+                await _storeBookingKeys(existingKeys);
+
+                setState(() {
+                  bookings[index] = true;
+                });
+              },
+              hasBooking: existingKeys.containsKey(eventId),
+              bookingKey: existingKeys[eventId] ?? '', // Pass the booking key to EventCard
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+class EventCard extends StatelessWidget {
+  final String eventName;
+  final bool isBooked;
+  final bool hasBooking;
+  final VoidCallback onBookPressed;
+  final String bookingKey;
+
+  const EventCard({
+    required this.eventName,
+    required this.isBooked,
+    required this.hasBooking,
+    required this.onBookPressed,
+    required this.bookingKey,
+  });
+
+  void _showTicketDetails(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Ticket Details"),
+          content: Text("Event: $eventName\nBooking Key: $bookingKey"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color.fromRGBO(246, 244, 235, 1),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 0, right: 12, top: 5),
+                  child: Image.network(
+                    "https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80",
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+                Flexible(
+                  child: Text(
+                    eventName,
+                    softWrap: true,
+                    maxLines: 2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: !isBooked && !hasBooking ? onBookPressed : null,
+                child: Text(isBooked ? "Scan QR" : "Book Seat"),
+              ),
+              ElevatedButton(
+                onPressed: hasBooking ? () => _showTicketDetails(context) : null,
+                child: Text(hasBooking ? "View Ticket" : "Not Booked"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
